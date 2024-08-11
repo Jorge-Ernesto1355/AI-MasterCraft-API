6b7e137b09285e53f38453abae35dbb72dd9b321
Jorge Ernesto <jorgais.ernesto@gmail.com>
2024-08-11 16:34:59 -0700

    feat: :sparkles: user domain

---
+++ src/users/domain/entities/User.ts
@@ -0,0 +1,66 @@
+import { Email } from "../values/Email";
+import { Password } from "../values/Password";
+
+export class User {
+  private readonly _id: string;
+  private readonly username: string;
+  private readonly email: Email;
+  private password: Password;
+
+  constructor(_id: string, username: string, email: Email, password: Password) {
+    this._id = _id;
+    this.email = email;
+    this.username = username;
+    this.password = password;
+  }
+
+  public static async create(
+    _id: string,
+    username: string,
+    email: string,
+    plainTextPassword: string
+  ): Promise<User> {
+    const emailObj = new Email(email);
+    const passwordObj = await Password.create(plainTextPassword);
+    return new User(_id, username, emailObj, passwordObj);
+  }
+
+  public getId() {
+    return this._id;
+  }
+
+  public getUsername() {
+    return this.username;
+  }
+
+  public getPassword() {
+    return this.password.getPassword();
+  }
+  public getEmail() {
+    return this.email.getEmail();
+  }
+
+  public static isValidEmail(email: string): boolean {
+    return Email.isValid(email);
+  }
+
+  public static isValidPassword(password: string) {
+    return Password.validatePassword(password);
+  }
+
+  public comparePassword(password: string) {
+    return this.password.comparePassword(password);
+  }
+
+  public async updatePassword(newPlainTextPassword: string): Promise<void> {
+    this.password = await Password.create(newPlainTextPassword);
+  }
+
+  public toJSON(): object {
+    return {
+      id: this._id,
+      username: this.username,
+      email: this.getEmail(),
+    };
+  }
+}
