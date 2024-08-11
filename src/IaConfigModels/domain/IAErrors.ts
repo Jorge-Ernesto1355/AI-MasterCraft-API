365bcafb6b183cb1132ae5ed6690449540b82a46
Jorge Ernesto <jorgais.ernesto@gmail.com>
2024-08-11 16:10:07 -0700

    feat: :sparkles: IAError
    instance of customError to validate if IA is found

---
+++ src/IaConfigModels/domain/IAErrors.ts
@@ -0,0 +1,4 @@
+import { createCustomError } from "../../utilities/ErrorFactory";
+
+export const IA_NOT_FOUND_MSG = "IA_NOT_FOUND";
+export const IA_NOT_FOUND = createCustomError(IA_NOT_FOUND_MSG);
