3ec8985966aef1684dabd7b124040c36b059c002
Jorge Ernesto <jorgais.ernesto@gmail.com>
2024-08-11 15:52:03 -0700

    feat: :sparkles: controller generateIA

---
+++ src/IaConfigModels/application/GenerateIA.ts
@@ -0,0 +1,52 @@
+import { Request, response, Response } from "express";
+import { iaServiceFactory } from "../infrastructure/dependecies";
+import { StatusCodes } from "http-status-codes";
+
+export class GenerateIA {
+  async run(req: Request, res: Response) {
+    try {
+      const { projectId } = req.params;
+      if (!projectId) throw new Error("Arguments must be defined");
+
+      const { prompt, userId } = this.validateString(
+        req.query.prompt,
+        req.query.userId
+      );
+
+      const projectIaService = await iaServiceFactory.create(userId);
+
+      if (projectIaService instanceof Error)
+        throw new Error(projectIaService.message);
+
+      const OutPutIA = await projectIaService.generateIA(projectId, prompt);
+
+      return res.status(StatusCodes.OK).json(OutPutIA);
+    } catch (error) {
+      return this.handleError(error, response);
+    }
+  }
+
+  private validateString(prompt: any, userId: any) {
+    try {
+      if (!prompt || !userId) throw new Error("Arguments must be defined");
+
+      if (typeof prompt !== "string")
+        throw new Error("prompt must be a string");
+      if (typeof userId !== "string")
+        throw new Error("userId must be a string");
+
+      return { prompt, userId };
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  private handleError(error: unknown, res: Response): Response {
+    if (error instanceof Error) {
+      return res.status(StatusCodes.BAD_REQUEST).json({ error: error.message });
+    }
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: "Internal Server Error" });
+  }
+}
