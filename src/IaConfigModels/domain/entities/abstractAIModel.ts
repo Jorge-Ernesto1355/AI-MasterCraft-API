bf59995801d92af27f8f230534f9c5b27ec51eca
Jorge Ernesto <jorgais.ernesto@gmail.com>
2024-08-11 16:12:01 -0700

    feat: :sparkles: Abstact IA model
    mold to class should keep im Ia Model

---
+++ src/IaConfigModels/domain/entities/abstractAIModel.ts
@@ -0,0 +1,44 @@
+export interface AIModelInput {
+  [key: string]: any;
+}
+
+export interface AIModelOutput {
+  [key: string]: any;
+}
+
+export interface AIModelConfig {
+  [key: string]: unknown;
+}
+
+export interface AIModelInput {
+  prompt: string;
+  config?: AIModelConfig;
+}
+
+export abstract class AbstractAIModel {
+  constructor(
+    protected readonly organization: string,
+    protected readonly modelName: string
+  ) {}
+
+  protected abstract prepareInput(input: AIModelInput): Record<string, unknown>;
+
+  abstract generate(
+    prompt: string,
+    config?: AIModelConfig
+  ): Promise<AIModelOutput>;
+
+  protected abstract run(input: AIModelInput): Promise<AIModelOutput>;
+
+  protected abstract stream(input: AIModelInput): Promise<AIModelOutput>;
+
+  protected getModelString(): string {
+    return `${this.organization}/${this.modelName}`;
+  }
+
+  protected shouldUseStreaming(): boolean {
+    return ["meta", "mistralai"].some((org) =>
+      this.organization.toLowerCase().includes(org)
+    );
+  }
+}
