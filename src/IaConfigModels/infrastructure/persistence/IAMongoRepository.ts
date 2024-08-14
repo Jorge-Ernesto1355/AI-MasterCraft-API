94081aaca212f59f8ee6ef97398e24649a577a13
Jorge Ernesto <jorgais.ernesto@gmail.com>
2024-08-14 15:16:23 -0700

    feat: :sparkles: add get projects and available models

--- src/IaConfigModels/infrastructure/persistence/IAMongoRepository.ts
+++ src/IaConfigModels/infrastructure/persistence/IAMongoRepository.ts
@@ -3,16 +3,35 @@ import { IA_NOT_FOUND, IA_NOT_FOUND_MSG } from "../../domain/IAErrors";
 import { IARepository } from "../../domain/interfaces/IARepository.interface";
 import { Project } from "../../domain/interfaces/Project.interface";
 import { ProjectIaMapper } from "../mappers/projectIaMapper";
+import AIModel from "./modelsAI";
 import projectIASchema from "./ProjectIASchema";
 
 export class IAMongoRepository implements IARepository {
+  async getAvailableIA(AIType: string) {
+    try {
+      const normalizedAIType = AIType.trim().toLowerCase();
+
+      const AIModels = await AIModel.find({
+        IAType: { $regex: new RegExp(`^${normalizedAIType}$`, "i") },
+      });
+
+      return AIModels;
+    } catch (error) {
+      throw error;
+    }
+  }
   async getIAById(IdIA: string): Promise<ProjectIa | Error> {
     try {
       const projectIa = await projectIASchema.findById(IdIA);
 
       if (!projectIa) return new IA_NOT_FOUND(IA_NOT_FOUND_MSG);
 
-      const projectToDomain = ProjectIaMapper.toDomain(projectIa);
+      const AImodel = await AIModel.findById(projectIa.AImodelId);
+
+      const projectToDomain = ProjectIaMapper.toDomain({
+        ...projectIa.toObject(),
+        ...AImodel?.toObject(),
+      });
 
       return projectToDomain;
     } catch (error) {
@@ -22,18 +41,42 @@ export class IAMongoRepository implements IARepository {
 
   async save(project: Project): Promise<ProjectIa> {
     try {
-      const { projectName, description, organization, modelName, config } =
-        project;
+      const { projectName, description, AImodelId, userId, config } = project;
       const newProject = await projectIASchema.create({
         projectName,
         description,
-        organization,
-        modelName,
+        AImodelId,
+        userId,
         config,
       });
-      const savedProject = await newProject.save();
-      const projectToDomain = ProjectIaMapper.toDomain(savedProject);
+
+      const AImodelObject = await AIModel.findById(AImodelId);
+
+      if (AImodelObject instanceof Error) throw new Error("AI model Error");
+
+      await newProject.save();
+
+      const entityModel = {
+        ...newProject.toObject(),
+        ...AImodelObject?.toObject(),
+      };
+
+      const projectToDomain = ProjectIaMapper.toDomain(entityModel);
+
       return projectToDomain;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getProjects(userId: string): Promise<Array<ProjectIa>> {
+    try {
+      const projects = await projectIASchema.find({ userId });
+      if (projects.length <= 0) return [];
+      const projectsToDomain = projects.map((project) => {
+        return ProjectIaMapper.toDomain(project);
+      });
+      return projectsToDomain;
     } catch (error) {
       throw error;
     }
