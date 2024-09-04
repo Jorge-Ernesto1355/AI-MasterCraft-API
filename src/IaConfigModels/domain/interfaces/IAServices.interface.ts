28caf2abc64a36af3648165cb64ee55e0b83c09f
Jorge Ernesto <jorgais.ernesto@gmail.com>
2024-09-04 06:13:58 -0700

    remove generateAI service

--- src/IaConfigModels/domain/interfaces/IAServices.interface.ts
+++ src/IaConfigModels/domain/interfaces/IAServices.interface.ts
@@ -3,7 +3,6 @@ import { Project } from "./Project.interface";
 export interface AIService {
   getById(projectId: string): Promise<Project | Error>;
   save(project: Project): Promise<Project>;
-  generateIA(projectId: string, prompt: string): Promise<Object>;
   getProjects(userId: string): Promise<Array<Project>>;
   getAvailableIA(AIType: string): Promise<Object>;
 }
