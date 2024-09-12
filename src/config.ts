a1aa7dcb5b6e4f6300dc2c6acc40755ed42c54c2
Jorge Ernesto <jorgais.ernesto@gmail.com>
2024-09-11 21:17:54 -0700

    feat: add route with config and body parser

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,13 @@
 import express, { Application } from "express";
+import { IncomingMessage, ServerResponse } from "http";
 import cors from "cors";
 import { corsOptions } from "./utilities/corsOptions";
 import dotenv from "dotenv";
 import { mongoDB } from "./db/mongoDb";
 import { userRouter } from "./users/infrastructure/userRoute.routes";
-import IARouter from "./IaConfigModels/infrastructure/projectIA.routes";
+import IARouter from "./IaConfigModels/infrastructure/https/projectIA.routes";
+import MessageRouter from "./IaConfigModels/infrastructure/https/Message.routes";
+import bodyParser from "body-parser";
 
 dotenv.config();
 
@@ -24,11 +27,31 @@ export class server {
     this.app.use(express.json({ limit: "5mb" }));
     this.app.use(express.urlencoded({ limit: "5mb", extended: true }));
     this.app.use(cors(corsOptions));
+    this.app.use(
+      bodyParser.json({
+        strict: true,
+        verify: (
+          req: IncomingMessage,
+          res: ServerResponse,
+          buf: Buffer,
+          encoding: string
+        ) => {
+          try {
+            JSON.parse(buf.toString(encoding as BufferEncoding)); // Verificar si el JSON es válido
+          } catch (e) {
+            res.statusCode = 400;
+            res.end("Invalid JSON"); // Responder con 400 si el JSON es inválido
+            throw new Error("Invalid JSON"); // Lanzar un error si el JSON es inválido
+          }
+        },
+      })
+    );
   }
 
   private routers() {
     this.app.use("/users", userRouter);
     this.app.use("/projectIa", IARouter);
+    this.app.use("/message", MessageRouter);
   }
 
   private async runDB() {
