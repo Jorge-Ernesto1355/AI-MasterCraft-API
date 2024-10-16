f56f6bf765ec4654188b80cc2ea3618ff9270fea
Jorge Ernesto <jorgais.ernesto@gmail.com>
2024-10-16 15:45:17 -0700

    Fix typo in IAModelRepository interface return type

--- src/IaConfigModels/domain/interfaces/IAModelRepository.interface.ts
+++ src/IaConfigModels/domain/interfaces/IAModelRepository.interface.ts
@@ -1,6 +1,6 @@
 import { AIModel } from "../entities/IAModel";
 
 export interface IAModelRepository {
-  findById(id: string): Promise<AIMode | null>;
+  findById(id: string): Promise<AIModel | null>;
   save(model: AIModel): Promise<void>;
 }
