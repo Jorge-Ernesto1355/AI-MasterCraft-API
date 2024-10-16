562a10c6529753df72b3d683a6dc98709dbd9765
Jorge Ernesto <jorgais.ernesto@gmail.com>
2024-10-16 15:44:23 -0700

    Update API route paths to include versioning prefix '/api/v1'

--- src/config.ts
+++ src/config.ts
@@ -49,9 +49,9 @@ export class server {
   }
 
   private routers() {
-    this.app.use("/users", userRouter);
-    this.app.use("/projectIa", IARouter);
-    this.app.use("/message", MessageRouter);
+    this.app.use("/api/v1/users", userRouter);
+    this.app.use("/api/v1/projectIa", IARouter);
+    this.app.use("/api/v1/message", MessageRouter);
   }
 
   private async runDB() {
