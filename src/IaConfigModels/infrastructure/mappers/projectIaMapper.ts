079d327477f0e88ff3b977c09e3b7097837b2e4f
Jorge Ernesto laptop <jorgais.ernesto@gmail.com>
2024-10-25 07:02:51 -0700

    add the _id property and change to modeId

--- src/IaConfigModels/infrastructure/mappers/projectIaMapper.ts
+++ src/IaConfigModels/infrastructure/mappers/projectIaMapper.ts
@@ -3,6 +3,7 @@ import ProjectIa from "../../domain/entities/ProjectAI";
 export class ProjectIaMapper {
   static toPersistence(projectIa: ProjectIa): any {
     return {
+      _id: projectIa._id,
       projectName: projectIa.projectName,
       description: projectIa.description,
       organization: projectIa.organization,
@@ -14,6 +15,7 @@ export class ProjectIaMapper {
 
   static toDomain(entity: any): ProjectIa {
     return new ProjectIa({
+      _id: entity._id,
       projectName: entity.projectName,
       description: entity.description,
       organization: entity.organization,
@@ -21,7 +23,7 @@ export class ProjectIaMapper {
       config: entity.config,
       modelType: entity.modelType,
       userId: entity.userId,
-      AImodelId: entity.AImodelId,
+      modelId: entity.modelId,
     });
   }
 }
