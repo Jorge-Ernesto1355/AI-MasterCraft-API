cb3d62cfa118ee432871517433abe007762b3131
Jorge Ernesto laptop <jorgais.ernesto@gmail.com>
2024-11-05 09:09:52 -0800

    add new service search by model name

--- src/IaConfigModels/application/index.ts
+++ src/IaConfigModels/application/index.ts
@@ -4,6 +4,7 @@ import { GetAvailableIA } from "./GetAvailableIA";
 import { GetIAById } from "./GetIAById";
 import { GetMessages } from "./getMessages";
 import { GetProjects } from "./GetProjects";
+import { SearchModelByName } from "./searchModelByName";
 
 const getById = new GetIAById();
 const createProjectAI = new CreateProjectAI();
@@ -11,6 +12,7 @@ const generateIA = new GenerateIA();
 const getProjects = new GetProjects();
 const getAvailableIA = new GetAvailableIA();
 const getMessages = new GetMessages();
+const searchModelByName = new SearchModelByName()
 export {
   getById,
   createProjectAI,
@@ -18,4 +20,5 @@ export {
   getProjects,
   getAvailableIA,
   getMessages,
+  searchModelByName
 };
