f4388069971c33d49d492121970ca25a057d3e67
Jorge Ernesto laptop <jorgais.ernesto@gmail.com>
2024-11-05 09:12:41 -0800

    add new route serach by name

--- src/IaConfigModels/infrastructure/projectIA.routes.ts
+++ src/IaConfigModels/infrastructure/projectIA.routes.ts
@@ -5,6 +5,7 @@ import {
   getAvailableIA,
   getById,
   getProjects,
+  searchModelByName,
 } from "../application";
 
 const IARouter = Router();
@@ -21,5 +22,7 @@ IARouter.get(
 IARouter.post("/:userId", createProjectAI.run.bind(createProjectAI));
 
 IARouter.post("/generateIA/:projectId", generateIA.run.bind(generateIA));
+
+IARouter.get('/search/:userId', searchModelByName.run.bind(searchModelByName))
 
 export default IARouter;
