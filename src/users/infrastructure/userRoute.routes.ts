bbb394ab288e3301940bf8754f6753fee12864fd
Jorge Ernesto <jorgais.ernesto@gmail.com>
2024-11-06 23:49:05 -0700

    add new route refreshtoken

--- src/users/infrastructure/userRoute.routes.ts
+++ src/users/infrastructure/userRoute.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getUserbyId, login, register } from "../application";
+import { getUserbyId, login, refreshToken, register } from "../application";
 
 const userRouter = Router();
 
@@ -8,5 +8,7 @@ userRouter.get("/:userId", getUserbyId.run.bind(getUserbyId));
 userRouter.post("/register", register.run.bind(register));
 
 userRouter.post("/login", login.run.bind(login));
+
+userRouter.get("/refreshToken/:userId", refreshToken.run.bind(refreshToken));
 
 export { userRouter };
