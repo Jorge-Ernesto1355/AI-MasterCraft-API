028d33afe6e4c1dcd17431b340c75fbbc012fe51
Jorge Ernesto <jorgais.ernesto@gmail.com>
2024-11-06 23:48:02 -0700

    change return of verifyToken

--- src/users/domain/interfaces/authRepository.interface.ts
+++ src/users/domain/interfaces/authRepository.interface.ts
@@ -1,5 +1,7 @@
+import { TokenVerificationResult } from "../../infrastructure/auth/tokenService.inteface";
+
 export interface authRepository {
   generateAccessToken(userId: string): Promise<string | Error>;
   generateRefreshToken(userId: string): Promise<string | Error>;
-  verifyToken(token: string): Promise<string | Error>;
+  verifyToken(token: string): TokenVerificationResult;
 }
