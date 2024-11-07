80b7810dafac66b0dd01aa82b9be6775c328cbe1
Jorge Ernesto <jorgais.ernesto@gmail.com>
2024-11-06 23:47:20 -0700

    remove console log

--- src/IaConfigModels/infrastructure/mappers/AImodelMapper.ts
+++ src/IaConfigModels/infrastructure/mappers/AImodelMapper.ts
@@ -1,33 +1,26 @@
 import { IAModel, IAModelDTO } from "../../domain/entities/IAModel";
 
-
 export class AImodelMapper {
-    static toPersistence(model: IAModel): IAModelDTO  {
-        return model.toJSON()
-    }
+  static toPersistence(model: IAModel): IAModelDTO {
+    return model.toJSON();
+  }
 
+  static toDomain(entity: any): IAModel {
+    const rawEntity = Array.isArray(entity) ? entity[0] : entity;
 
-    static toDomain(entity: any): IAModel {
-        
-        
-        const rawEntity = Array.isArray(entity) ? entity[0] : entity;
-        
-        const id = rawEntity._id ? rawEntity._id.toString() : rawEntity.id;
+    const id = rawEntity._id ? rawEntity._id.toString() : rawEntity.id;
 
-        console.log(rawEntity.imageUrl, "rawEntity")
-        return new IAModel({
-            id,
-            modelName: rawEntity.modelName,
-            modelType: rawEntity.modelType,
-            organization: rawEntity.organization,
-            config: rawEntity.configuration || rawEntity.config,
-            imageUrl: rawEntity.imageUrl,
-                
-        });
-    }
+    return new IAModel({
+      id,
+      modelName: rawEntity.modelName,
+      modelType: rawEntity.modelType,
+      organization: rawEntity.organization,
+      config: rawEntity.configuration || rawEntity.config,
+      imageUrl: rawEntity.imageUrl,
+    });
+  }
 
-    
-    static toDomainList(entities: any[]): IAModel[] {
-        return entities.map(entity => this.toDomain(entity));
-    }
+  static toDomainList(entities: any[]): IAModel[] {
+    return entities.map((entity) => this.toDomain(entity));
+  }
 }
