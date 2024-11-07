295761da1b7df933a0a07563fe2594c1f1c6051b
Jorge Ernesto <jorgais.ernesto@gmail.com>
2024-11-06 23:48:20 -0700

    add method refreshToken

--- src/users/domain/interfaces/AuthService.interface.ts
+++ src/users/domain/interfaces/AuthService.interface.ts
@@ -1,5 +1,7 @@
+import { TokenVerificationResult } from "../../infrastructure/auth/tokenService.inteface";
+
 export interface IAuthenticationService {
   generateAccessToken(userId: string): Promise<string | Error>;
   generateRefreshToken(userId: string): Promise<string | Error>;
-  verifyToken(token: string): Promise<string | Error>;
+  verifyToken(token: string): TokenVerificationResult;
 }
