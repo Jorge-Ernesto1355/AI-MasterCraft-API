13b90a11a485e2080ab8ec8ba71aefc11df21b94
Jorge Ernesto <jorgais.ernesto@gmail.com>
2024-11-06 23:49:42 -0700

    add interface to token verification result

--- src/users/infrastructure/auth/tokenService.inteface.ts
+++ src/users/infrastructure/auth/tokenService.inteface.ts
@@ -1,5 +1,18 @@
 export interface tokenService {
   generateAccessToken(userId: string): string;
   generateRefreshToken(userId: string): string;
-  verifyToken(token: string): Promise<string | Error>;
+  verifyToken(token: string): TokenVerificationResult;
+}
+
+export interface TokenVerificationResult {
+  isValid: boolean;
+  userId?: string;
+  error?: string;
+}
+
+export interface tokenPayload {
+  userId: string;
+  email?: string;
+  iat?: number;
+  exp?: number;
 }
