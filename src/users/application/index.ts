64d94040b6c8dcc2fa9dcd1f2ea76dc025e117d8
Jorge Ernesto <jorgais.ernesto@gmail.com>
2024-11-06 23:47:42 -0700

    add new class get refresh token

--- src/users/application/index.ts
+++ src/users/application/index.ts
@@ -1,3 +1,4 @@
+import { getRefreshToken } from "./auth/getRefreshToken";
 import { Login } from "./auth/Login";
 import { Register } from "./auth/Register";
 import { GetUserById } from "./GetuserById";
@@ -5,5 +6,6 @@ import { GetUserById } from "./GetuserById";
 const getUserbyId = new GetUserById();
 const register = new Register();
 const login = new Login();
+const refreshToken = new getRefreshToken();
 
-export { getUserbyId, register, login };
+export { getUserbyId, register, login, refreshToken };
