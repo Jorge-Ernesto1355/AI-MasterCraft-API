bd3da72c01661f47928d74e371449df3dd55f358
Jorge Ernesto <jorgais.ernesto@gmail.com>
2024-11-10 22:17:35 -0700

    add search mode by name

--- src/IaConfigModels/application/searchModelByName.ts
+++ src/IaConfigModels/application/searchModelByName.ts
@@ -5,54 +5,55 @@ import { StatusCodes } from "http-status-codes";
 import { ApiError } from "../../utilities/apiError";
 
 export class SearchModelByName {
-    async  run(req: Request, response: Response){
-        
+  async run(req: Request, response: Response) {
+    try {
+      const { userId } = req.params;
+      const { query } = req.query;
 
-            try {
-                const {userId} = req.params
-                const {query} = req.query 
-                
-                const {userId: validatedUserId, query: validatedQuery} = this.validateInput(userId, query)
-                const service = await this.createIAService(validatedUserId)
-                const models = await service.searchModelByName(validatedQuery)
-                return response.status(StatusCodes.OK).json(models)
-            } catch (error) {
-              
-                this.handleError(error, response)
-            }
-       
+      const { userId: validatedUserId, query: validatedQuery } =
+        this.validateInput(userId, query);
+      const service = await this.createIAService(validatedUserId);
+      const models = await service.searchModelByName(validatedQuery);
+      return response.status(StatusCodes.OK).json(models);
+    } catch (error) {
+      return this.handleError(error, response);
+    }
+  }
 
+  private validateInput(userId: string, query: any) {
+    if (!userId || !query)
+      throw new Error(ErrorMessage.ParametersMustBeDefined);
+
+    if (typeof query !== "string")
+      throw new Error(ErrorMessage.ParameterMustBeString);
+
+    return { userId, query };
+  }
+
+  private async createIAService(userId: string) {
+    const IAService = await iaServiceFactory.create(userId);
+    if (IAService instanceof Error) {
+      throw new ApiError(
+        ErrorMessage.ErrorServiceCreation,
+        StatusCodes.FAILED_DEPENDENCY
+      );
+    }
+    return IAService;
+  }
+
+  private handleError(error: unknown, res: Response): Response {
+    if (error instanceof ApiError) {
+      return res.status(error.statusCode).json({
+        status: "error",
+        message: error.message,
+        code: error.name,
+      });
     }
 
-    private validateInput(userId: string, query: any) {
-    if(!userId || !query) throw new Error(ErrorMessage.ParametersMustBeDefined)
-    
-    if(typeof query !== "string") throw new Error(ErrorMessage.ParameterMustBeString)
-
-    return {userId, query}
-    }
-
-    private   async createIAService(userId: string) {
-        const IAService = await iaServiceFactory.create(userId);
-        if (IAService instanceof Error) {
-          throw new ApiError(ErrorMessage.ErrorServiceCreation, StatusCodes.FAILED_DEPENDENCY)
-        }
-        return IAService;
-      }
-
-      private handleError(error: unknown, res: Response): Response {
-        if (error instanceof ApiError) {
-          return res.status(error.statusCode).json({
-            status: "error",
-            message: error.message,
-            code: error.name,
-          });
-        }
-    
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-          status: "error",
-          message: "An unexpected error occurred",
-          code: "InternalServerError",
-        });
-      }
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      status: "error",
+      message: "An unexpected error occurred",
+      code: "InternalServerError",
+    });
+  }
 }
