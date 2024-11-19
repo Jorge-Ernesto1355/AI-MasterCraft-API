4baa884e3ce283171ead306062186cf724fb9fef
Jorge Ernesto <jorgais.ernesto@gmail.com>
2024-11-18 23:36:16 -0700

    use replicate adapter

--- src/IaConfigModels/domain/entities/IAModel.ts
+++ src/IaConfigModels/domain/entities/IAModel.ts
@@ -7,7 +7,6 @@ export interface IAModelDTO {
   modelName: string;
   modelType: string;
   imageUrl: string;
-
 }
 
 export class IAModel {
@@ -17,64 +16,60 @@ export class IAModel {
   private readonly config: Record<string, any>;
   private readonly id: string;
   private readonly imageUrl: string;
-  
 
   constructor(props: IAModelDTO) {
-      this.id = props.id ?? '';
-      this.organization = props.organization;
-      this.modelName = props.modelName;
-      this.modelType = props.modelType;
-      this.config = props.config;
-      this.imageUrl = props.imageUrl;
-      
+    this.id = props.id ?? "";
+    this.organization = props.organization;
+    this.modelName = props.modelName;
+    this.modelType = props.modelType;
+    this.config = props.config;
+    this.imageUrl = props.imageUrl;
   }
 
   async run(prompt: string) {
-      const replicateAdapter = new ReplicateAdapter(
-          this.organization,
-          this.modelName
-      );
-      return {
-          output: "hola este es mi primer mensaje si es que funciona",
-      };
+    const replicateAdapter = new ReplicateAdapter(
+      this.organization,
+      this.modelName
+    );
+
+    return {
+      output: "sisisiis",
+    };
   }
 
   public toJSON(): IAModelDTO {
-      return {
-          id: this.id,
-          modelType: this.modelType,
-          organization: this.organization,
-          modelName: this.modelName,
-          config: this.config,
-          imageUrl: this.imageUrl,
-          
-      };
+    return {
+      id: this.id,
+      modelType: this.modelType,
+      organization: this.organization,
+      modelName: this.modelName,
+      config: this.config,
+      imageUrl: this.imageUrl,
+    };
   }
 
   // Getters
   getId(): string {
-      return this.id;
+    return this.id;
   }
 
   getOrganization(): string {
-      return this.organization;
+    return this.organization;
   }
 
   getModelName(): string {
-      return this.modelName;
+    return this.modelName;
   }
 
   getModelType(): string {
-      return this.modelType;
+    return this.modelType;
   }
 
   getConfig(): Record<string, any> | undefined {
-      return this.config;
+    return this.config;
   }
 
   getImageUrl(): string | undefined {
-      return this.imageUrl;
+    return this.imageUrl;
   }
-
- 
 }
