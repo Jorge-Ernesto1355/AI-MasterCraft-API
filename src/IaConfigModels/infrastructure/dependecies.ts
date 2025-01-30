a1027b5f4c1e412eadfd7e2a5af324f1e7516e7e
Jorge Ernesto <jorgais.ernesto@gmail.com>
2025-01-30 14:58:26 -0700

    feat: :sparkles: new dependecy resolve instead new instance

--- src/IaConfigModels/infrastructure/dependecies.ts
+++ src/IaConfigModels/infrastructure/dependecies.ts
@@ -1,28 +1,7 @@
-import { userRepository as UserRepository } from "../../users/domain/repositories/userRepository";
-import { mongoRepository as UserMongoRepository } from "../../users/infrastructure/persistence/mongoRepository";
+import { container } from "tsyringe";
 import { MessageService } from "../application/services/MessageService";
 import { IaServiceFactory } from "../domain/IAService";
-import { IARepository } from "../domain/repositories/IARepositories";
-import { messageRepository as MessageRepository } from "../domain/repositories/messageRepository";
-import { IAMongoRepository } from "./persistence/IAMongoRepository";
-import { messageMongoRepository as MessageMongoRepository } from "./persistence/messageMongoRepository";
-
-//Persistence
-const iaMongoRepository = new IAMongoRepository();
-const userMongoRepository = new UserMongoRepository();
-const messageMongoRepository = new MessageMongoRepository();
-
-//Repositories
-const userRepository = new UserRepository(userMongoRepository);
-const iaRepository = new IARepository(iaMongoRepository);
-const messageRepository = new MessageRepository(messageMongoRepository);
 
 //Servicies
-export const messageService = new MessageService(
-  messageRepository,
-  iaRepository
-);
-export const iaServiceFactory = new IaServiceFactory(
-  userRepository,
-  iaRepository
-);
+export const messageService = container.resolve(MessageService);
+export const iaServiceFactory = container.resolve(IaServiceFactory);
